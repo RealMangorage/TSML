670fb50433384c663008e4de518b1e2b10586755
RealMangoRage <64402114+RealMangorage@users.noreply.github.com>
2026-03-07 21:11:11 -0800

    Use TSMLConstants class

--- Loader/src/main/java/org/mangorage/tsml/internal/mod/BuiltInMod.java
+++ Loader/src/main/java/org/mangorage/tsml/internal/mod/BuiltInMod.java
@@ -1,6 +1,7 @@
 package org.mangorage.tsml.internal.mod;
 
 import org.mangorage.tsml.api.mod.Mod;
+import org.mangorage.tsml.internal.core.TSMLConstants;
 
-@Mod(id = "tsml")
+@Mod(id = TSMLConstants.TSML_MOD_ID)
 public final class BuiltInMod { }
