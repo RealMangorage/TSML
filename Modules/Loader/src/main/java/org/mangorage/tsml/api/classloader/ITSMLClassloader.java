340fa309f12c986e3fdbf5ad086a1e6299111ffc
RealMangoRage <64402114+RealMangorage@users.noreply.github.com>
2026-03-18 04:20:26 -0700

    Fixed buildscript issue.

--- Modules/Loader/src/main/java/org/mangorage/tsml/api/classloader/ITSMLClassloader.java
+++ Modules/Loader/src/main/java/org/mangorage/tsml/api/classloader/ITSMLClassloader.java
@@ -1,9 +1,6 @@
 package org.mangorage.tsml.api.classloader;
 
-import org.mangorage.jar.IJar;
-
 import java.net.URL;
-import java.util.List;
 
 public interface ITSMLClassloader {
     byte[] getClassBytes(String name);
